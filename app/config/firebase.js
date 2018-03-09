91ed7e5ef48447532e88b7e73fdfd6cb42b20d40
shaggyshelar <shaggy.shelar@gmail.com>
2018-03-09 11:21:11 +0530

    Updated icons

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -1,13 +1,22 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 
+// firebase.initializeApp({
+//     apiKey: 'AIzaSyDM4bfZawYFEvNsYlNyNZLEfTPSPsbtUkQ',
+//     authDomain: 'tiecon-b3493.firebaseapp.com',
+//     databaseURL: 'https://tiecon-b3493.firebaseio.com',
+//     projectId: 'tiecon-b3493',
+//     storageBucket: 'tiecon-b3493.appspot.com',
+//     messagingSenderId: '489302991624'
+// });
+
 firebase.initializeApp({
-    apiKey: 'AIzaSyDM4bfZawYFEvNsYlNyNZLEfTPSPsbtUkQ',
-    authDomain: 'tiecon-b3493.firebaseapp.com',
-    databaseURL: 'https://tiecon-b3493.firebaseio.com',
-    projectId: 'tiecon-b3493',
-    storageBucket: 'tiecon-b3493.appspot.com',
-    messagingSenderId: '489302991624'
+    apiKey: "AIzaSyAmhu_J_9kRqDuQZox7ccZVNgnOA9fc4Gw",
+    authDomain: "tie-con-management.firebaseapp.com",
+    databaseURL: "https://tie-con-management.firebaseio.com",
+    projectId: "tie-con-management",
+    storageBucket: "tie-con-management.appspot.com",
+    messagingSenderId: "852890830155"
 });
 
 export default firebase;
